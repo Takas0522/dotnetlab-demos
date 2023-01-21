8d8f749fc07af69328f84674db131114cfd06f95
Takas0522 <t_o_19880522@outlook.com>
2023-01-21 12:56:32 +0900

    init

--- 2023/01-try-mono-lib/nx/libs/@devtakas/utility/src/lib/const/mock-settings.token.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { MockSetting } from '../models/mock-setting.interface';
-
-export const MOCK_SETTINGS_TOKEN = new InjectionToken<MockSetting[]>(
-  'MockSettings'
-);
---
+++ 2023/01-try-mono-lib/nx/projects/devtakas/controls-two/src/lib/controls-two.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ControlsTwoComponent } from './controls-two.component';
+
+
+
+@NgModule({
+  declarations: [
+    ControlsTwoComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ControlsTwoComponent
+  ]
+})
+export class ControlsTwoModule { }
