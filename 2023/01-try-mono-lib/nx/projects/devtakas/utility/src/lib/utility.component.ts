04417ce39026d8c8e47258faffbfc6ae5706df94
Takas0522 <t_o_19880522@outlook.com>
2023-01-21 12:20:50 +0900

    init nx

---
+++ 2023/01-try-mono-lib/nx/projects/devtakas/utility/src/lib/utility.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-utility',
+  template: `
+    <p>
+      utility works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UtilityComponent {
+
+}
---
+++ 2023/01-try-mono-lib/nx/projects/devtakas/utility/src/lib/utility.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { UtilityComponent } from './utility.component';
+
+
+
+@NgModule({
+  declarations: [
+    UtilityComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    UtilityComponent
+  ]
+})
+export class UtilityModule { }
