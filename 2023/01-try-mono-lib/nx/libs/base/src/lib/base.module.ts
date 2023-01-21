a4f217e321dcde5ff631cdf26a8940a289e43a1e
Takas0522 <t_o_19880522@outlook.com>
2023-01-21 11:39:34 +0900

    add turbo

--- 2023/01-try-mono-lib/nx/libs/base/src/lib/base.module.ts
+++ 2023/01-try-mono-lib/nx/libs/base/src/lib/base.module.ts
@@ -3,10 +3,15 @@ import { BaseComponent } from './base.component';
 
 import { ControlsOneModule } from '@devtakas/controls-one';
 import { ControlsTwoModule } from '@devtakas/controls-two';
+import {  } from '@devtakas/sample';
 
 @NgModule({
   declarations: [BaseComponent],
   imports: [ControlsOneModule, ControlsTwoModule],
   exports: [BaseComponent],
 })
-export class BaseModule {}
+export class BaseModule {
+  constructor() {
+    
+  }
+}
