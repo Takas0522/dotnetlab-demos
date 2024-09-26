b26086e04f75419375e7c3553929bd721cd4bff0
Takas0522 <t_o_19880522@outlook.com>
2024-09-27 01:21:18 +0900

    add mpt-report-sample

--- 2024/09/front/src/app/controls/button/button.component.ts
+++ 2024/09/front/src/app/controls/button/button.component.ts
@@ -1,12 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Output() click: EventEmitter<void> = new EventEmitter<void>();
+  @Input() color: 'warn' | 'primary' = 'primary';
 }
