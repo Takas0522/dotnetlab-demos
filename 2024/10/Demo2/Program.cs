16ae8979f618a25f0a4eaaedf623b3cdddefa81d
Takas0522 <t_o_19880522@outlook.com>
2024-10-19 11:25:42 +0900

    Demo2Add

--- 2024/10/Demo2/Program.cs
+++ 2024/10/Demo2/Program.cs
@@ -6,36 +6,40 @@ using System.Text.Json;
 #pragma warning disable SKEXP0010 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
 internal class Program
 {
-    private static async Task Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var hostBuilder = DemoLib.DemoHost.ConfigureHostBuilder();
         var host = hostBuilder.Build();
         var kernel = host.Services.GetRequiredService<Kernel>();
 
-        // Eventのサンプル構造体を作成できるか試してみる。
+        // Eventのサンプル構造体に対してデータの不足が発生していた場合の挙動を確認する。
         var settings = new OpenAIPromptExecutionSettings
         {
             ResponseFormat = typeof(EventsResult)
         };
 
+        // Endが不足している
         var prompt = @"
-            イベントのサンプルデータのJSONを3個ほど構成してください。イベントはEvents配列に格納されます。
-            イベントの構成情報はタイトルのSubject、本文のBody、開始日時のStart、終了日時のEndを含む必要があります。
+            以降に記載される列挙情報を使用してイベントデータを作成してください。イベントはEvents配列に格納されます。
+            * subject: 新しい予定
+            * body: 本文
+            * start: 2022-01-01T00:00:00
+            * subject: 新しい予定2
+            * body: 本文2
+            * start: 2022-01-02T00:00:00
+            作成される構造体に不足している情報がある場合は 不足項目に **Error** と表示されます。
             ";
 
         var result = await kernel.InvokePromptAsync(prompt, new(settings));
 
+        if (result.ToString().Contains("Error"))
+        {
+            Console.WriteLine("データに不足があります。");
+            return;
+        }
         var eventsResult = JsonSerializer.Deserialize<EventsResult>(result.ToString());
 
-        eventsResult.Events.ForEach(e =>
-        {
-            Console.WriteLine("--------------------Event--------------------");
-            Console.WriteLine($"Subject: {e.Subject}");
-            Console.WriteLine($"Body: {e.Body}");
-            Console.WriteLine($"Start: {e.Start}");
-            Console.WriteLine($"End: {e.End}");
-        });
-
+        Console.WriteLine(eventsResult);
     }
 }
 #pragma warning restore SKEXP0010 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
