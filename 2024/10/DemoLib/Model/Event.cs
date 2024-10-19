b9e2243dff1cba1cd5dede181fc98946bfbfb054
Takas0522 <t_o_19880522@outlook.com>
2024-10-19 10:29:53 +0900

    Demoいっこめ

--- 2024/10/DemoLib/Model/Event.cs
+++ 2024/10/DemoLib/Model/Event.cs
@@ -6,14 +6,28 @@ using System.Threading.Tasks;
 
 namespace DemoLib.Model
 {
+    public class EventsResult
+    {
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+
     public class Event
     {
         public string Subject { get; set; } = "";
-        public Body? Body { get; set; }
-        public DateTimeSet? Start { get; set; }
-        public DateTimeSet? End { get; set; }
+        public string Body { get; set; } = "";
+        public string Start { get; set; } = "";
+        public string End { get; set; } = "";
 
     }
+
+    //public class Event
+    //{
+    //    public string Subject { get; set; } = "";
+    //    public Body? Body { get; set; }
+    //    public DateTimeSet? Start { get; set; }
+    //    public DateTimeSet? End { get; set; }
+
+    //}
 
     public class Body
     {
