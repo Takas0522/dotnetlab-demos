b67be85b74e7464c60b0e3bbb07f26180cb1dad8
Takas0522 <t_o_19880522@outlook.com>
2025-11-22 11:26:03 +0900

    add test

---
+++ 2025/11/src/AsciiToUtfEight.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace AsciiToUtfEight.Tests;
+
+public class ProgramTests : IDisposable
+{
+    [Fact]
+    public void HelloWorld_ShouldPass()
+    {
+        // Arrange
+        var expected = "Hello World";
+
+        // Act
+        var actual = "Hello World";
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    public void Dispose()
+    {
+    }
+}
